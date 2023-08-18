c2c7f166f90c71f5def52d85659fb2efb395f79c
Gašper Šavle <126724362+gaspersavle@users.noreply.github.com>
2023-08-18 12:44:42 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source "opt/ros/$ROS_DISTRO/setup.bash && source "$CATKIN_WS/devel/setup.bash"
+source "opt/ros/$ROS_DISTRO/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
+
 cd /root/catkin_ws
+
 exec "$@"
